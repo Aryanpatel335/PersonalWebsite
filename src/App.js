a0760bbbd6d6459d29da1871f5a1515326334e49
aryan <aryanp862002@gmail.com>
2022-08-31 14:33:42 -0400

    changed logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p className="line-1 anim-typewriter">hi my name is aryan patel...</p>
       </header>
     </div>
