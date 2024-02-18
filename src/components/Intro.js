987b65740fe794a671e7a2d1b1c0c4c32407ba32
Aryan Patel <aryanpatel@aryans-air.home>
2024-02-18 00:53:00 -0500

    centering image

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,22 +2,31 @@ import React from "react";
 import cartoonImage from "../resources/cartoon.png";
 import StockCanvas from "./StockCanvas";
 import Nav from "./Nav";
+
 const Intro = () => {
   return (
-    <div>
-      <Nav />
-      <div className="flex flex-col justify-center items-center h-dvh bg-black relative">
-        <div className="absolute inset-0 z-0">
+    <div className="relative h-screen bg-black">
+      {/* Overlay Nav without specifying a height, allowing content to center vertically in the remaining space */}
+      <div className="absolute top-0 left-0 z-20 w-full">
+        <Nav />
+      </div>
+      <div className="flex justify-center items-center h-full">
+        {/* StockCanvas is positioned absolutely to fill the entire screen but under the content */}
+        <div className="absolute top-0 left-0 right-0 bottom-0 z-0">
           <StockCanvas />
         </div>
-        <div className="justify-center z-10 flex items-center flex-col">
+        {/* Centered content accounting for Nav, using padding or margin if necessary */}
+        <div
+          className="z-10 flex flex-col items-center"
+          style={{ paddingTop: "var(--nav-height, 96px)" }}
+        >
           <img
             src={cartoonImage}
             alt="Cartoon representation"
-            className="z-10 justify-center items-center w-70 h-80 border-3 border-gray rounded-full mb-5"
+            className="w-70 h-80 border-3 border-gray rounded-full mb-5"
           />
-          <p className="z-10">hi, my name is aryan patel</p>
-          <p className="z-10">i am a software engineering student</p>
+          <p>hi, my name is aryan patel</p>
+          <p>i am a software engineering student</p>
         </div>
       </div>
     </div>
